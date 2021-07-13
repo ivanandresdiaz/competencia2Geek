0671e0a37b7eb1c63c9fcf54cff83ff85828667a
FriendGlak <camilojheans@gmail.com>
2021-07-13 11:12:23 -0500

    Obteniendo datos API

---
+++ src/components/CardHeroes.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const CardHeroes = ({ character }) => {
+    return (
+        <div>
+            {console.log("Pintando tarjetas")}
+            {
+                // character.map(char => (
+                //     <div className="" key={char.id}>
+                //         <img src={char.image} className="" alt={char.name}></img>
+                //         <p className="">{char.name}</p>
+                //         <p className="">{char.status}</p>
+                //         <p className="">{char.gender}</p>
+                //         <p className="">{char.origin}</p>
+                //         <button>Ver detalle</button>
+
+
+                //     </div>
+                // ))
+            }
+        </div>
+    )
+}
+
+export default CardHeroes
---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+import CardHeroes from '../../components/CardHeroes';
+
+const Home = () => {
+
+    const [character, setCharacter] = useState([]);
+
+
+    const getCharacters = async () => {
+        const url = "http://localhost:3000/heroes";
+        const resp = await fetch(url);
+        const data = await resp.json();
+        const { results } = data;
+        console.log(data)
+        
+        const characters = results.map((char) => {
+            return {
+                id: char.id,
+                name: char.name,
+                superhero: char.superhero,
+                publisher: char.publisher,
+                alter_ego: char.alter_ego,
+                first_appearance: char.first_appearance,
+                image: char.image
+            }
+
+        });
+        return characters
+
+    }
+
+
+    useEffect(() => {
+        getCharacters()
+            .then((resp) => {
+                setCharacter(resp)
+            })
+    }, [])
+
+    return (
+        <>
+            <h1>Súper Heroes</h1>
+            <hr></hr>
+            <CardHeroes />
+
+        </>
+    )
+}
+
+export default Home
